a5cecc7c1494b78b3967993c409590879a9cd52b
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2020-10-14 13:44:17 +0200

    Update examplePlotMacro.C

--- scripts/examplePlotMacro.C
+++ scripts/examplePlotMacro.C
@@ -9,37 +9,42 @@
 #include "TStyle.h"
 #include "TLatex.h"
 
-//#include <string>
+#include <string>
 #include <iostream>
 
-void examplePlotMacro() {
+void examplePlotMacro(const std::string& histogram, const histo) {
 
     TFile inFile("ggH_MS2_ctau10.root"); // read in file. Exercise: can this be done through a command line arguement? That way you don't have to edit code for each new histogram ...
     
     std::cout << "Enter in name of histogram: ";
-    //string histogram;
-    //std::cin >> histogram;
-    TH2I* h_VertexPosXY = (TH2I*)inFile.Get("h_VertexPosXY"); // Load in histogram
-    //TH1F* histogram = (TH1F*)inFile.Get("histogram");
+   
+    //TH2I* h_VertexPosXY = (TH2I*)inFile.Get("h_VertexPosXY"); // Load in histogram
+    TH1F* histo = (TH1F*)inFile.Get(histogram);
     
     TCanvas* canvy = new TCanvas ("canvy", "canvy", 50, 50, 800, 600); // Canvas to draw histogram on
     //gPad->SetLogy(); // set log scale for y-axis on for objects drawn on the canvas from this point on
 
-    //histogram->GetXaxis()->SetTitle("p_{T} GeV"); // set a title for the x-axis
-    //histogram->GetXaxis()->SetRangeUser(0.0, 200.); 
-    h_VertexPosXY->GetXaxis()->SetTitle("Vertex position x"); // set a title for the x-axis
-    h_VertexPosXY->GetXaxis()->SetRangeUser(-150., 150.); // change the x-axis range to 0-200
+    histo->GetXaxis()->SetTitle("p_{T} GeV"); // set a title for the x-axis
+    histo->GetXaxis()->SetRangeUser(0.0, 200.); 
+    //h_VertexPosXY->GetXaxis()->SetTitle("Vertex position x"); // set a title for the x-axis
+    //h_VertexPosXY->GetXaxis()->SetRangeUser(-150., 150.); // change the x-axis range to 0-200
     //h_VertexPosXY->GetXaxis()->SetRangeUser(0.0, 200.); // change the x-axis range to 0-200
     
-    h_VertexPosXY->GetYaxis()->SetTitle("Vertex position y"); // set a title for the x-axis
-    h_VertexPosXY->GetYaxis()->SetRangeUser(-150., 150.); // change the x-axis range to 0-200
+    //h_VertexPosXY->GetYaxis()->SetTitle("Vertex position y"); // set a title for the x-axis
+    //h_VertexPosXY->GetYaxis()->SetRangeUser(-150., 150.); // change the x-axis range to 0-200
 
-    //histogram->Draw();
-    h_VertexPosXY->Draw(); // draw histo on canvas
+    histo->Draw();
+    //h_VertexPosXY->Draw(); // draw histo on canvas
     canvy->Draw(); // draw canvas!
 
     // save canvas with drawn histogram
-    canvy->SaveAs("h_VertexPosXY.gif"); // .pdf and other formats work too!
-    canvy->SaveAs("histogram"); 
+    canvy->SaveAs(histogram.gif); // .pdf and other formats work too!
+    canvy->SaveAs(histogram); 
     delete canvy;
+}
+main()
+{
+    string histogram;
+    std::cin >> histogram;
+    std::cin >> histo
 }
