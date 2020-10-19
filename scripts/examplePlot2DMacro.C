4345ae752e8e49cc6c2a1d0a2cc491fa1c9b424c
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2020-10-19 20:53:26 +0200

    trying to make 2D manipulating plot

---
+++ scripts/examplePlot2DMacro.C
@@ -0,0 +1,31 @@
+//An example macro for manipulating 2D plots
+
+ #include "TFile.h"
+
+ #include "TCanvas.h"
+ #include "TROOT.h"
+ #include "TH1F.h"
+ #include "TLegend.h"
+ #include "TStyle.h"
+ #include "TLatex.h"
+
+  void examplePlot2DMacro() {  
+  
+     TFile inFile("ggH_MS2_ctau10.root"); // read in file. Exercise: can this be done through a command line arguement? That way you don't have to edit code for each new histogram ...
+     TH2I* h_VertexPosXY = (TH1F*)inFile.Get("VertexPosXY"); // Load in histogram
+   
+     TCanvas* canv2 = new TCanvas ("canv2", "canv2", 50, 50, 800, 600); // Canvas to draw histogram on
+  
+     gPad->SetLogy(); // set log scale for y-axis on for objects drawn on the canvas from this point on
+
+     h_VertexPosXY->GetXaxis()->SetTitle("Vertex Position X"); // set a title for the x-axis
+     h_VertexPosXY->GetXaxis()->SetRangeUser(0.0, 200.); // change the x-axis range to 0-200
+     h_VertexPosXY->GetYaxis()->SetTitle("Vertex Position Y"); 
+     h_VertexPosXY->GetYaxis()->SetRangeUser(0.0, 200.); //
+ 
+      h_VertexPosXY->Draw("colz"); // draw histo on canvas
+     canv2->Draw(); // draw canvas!
+
+     // save canvas with drawn histogram
+     canv2->SaveAs(" h_VertexPosXY.gif"); // .pdf and other formats work too! 
+     }
