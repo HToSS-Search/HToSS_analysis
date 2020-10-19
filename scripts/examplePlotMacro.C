0df3f6007332a0c01d2390fc56c4d3d2e009e998
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2020-10-19 20:12:35 +0200

    try for 2 histograms

--- scripts/examplePlotMacro.C
+++ scripts/examplePlotMacro.C
@@ -9,12 +9,10 @@
  #include "TStyle.h"
  #include "TLatex.h"
 
-int main(){
- 
-  void examplePlotMacro(std::string histo) {  
+  void examplePlotMacro() {  
   
      TFile inFile("ggH_MS2_ctau10.root"); // read in file. Exercise: can this be done through a command line arguement? That way you don't have to edit code for each new histogram ...
-     TH1F* h_genParPt = (TH1F*)inFile.Get(histo); // Load in histogram
+     TH1F* h_genParPt = (TH1F*)inFile.Get("h_genParPt"); // Load in histogram
 
      TCanvas* canvy = new TCanvas ("canvy", "canvy", 50, 50, 800, 600); // Canvas to draw histogram on
      gPad->SetLogy(); // set log scale for y-axis on for objects drawn on the canvas from this point on
@@ -26,14 +24,24 @@ int main(){
      canvy->Draw(); // draw canvas!
 
      // save canvas with drawn histogram
-     canvy->SaveAs(histo".gif"); // .pdf and other formats work too!
+     canvy->SaveAs("h_genParPt.gif"); // .pdf and other formats work too!
+
+     delete canvy;
+   
+     TH1F* h_genParEta = (TH1F*)inFile.Get("h_genParEta"); // Load in histogram
+
+     TCanvas* canvy = new TCanvas ("canvy", "canvy", 50, 50, 800, 600); // Canvas to draw histogram on
+     gPad->SetLogy(); // set log scale for y-axis on for objects drawn on the canvas from this point on
+
+     h_genParPt->GetXaxis()->SetTitle("#eta"); // set a title for the x-axis
+     h_genParPt->GetXaxis()->SetRangeUser(0.0, 200.); // change the x-axis range to 0-200
+
+     h_genParPt->Draw("colz"); // draw histo on canvas
+     canvy->Draw(); // draw canvas!
+
+     // save canvas with drawn histogram
+     canvy->SaveAs("h_genParEta.gif"); // .pdf and other formats work too!
 
      delete canvy;
      }
- 
-     std::string histogram;
-     std::cin>>histogram;
-     examplePlotMacro(histogram)
-      
-      
-}
+
