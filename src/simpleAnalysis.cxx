00725e08061e28cf038b5f87e6bc4a3ddf738c8b
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2020-10-25 14:11:06 +0100

    Update simpleAnalysis.cxx

--- src/simpleAnalysis.cxx
+++ src/simpleAnalysis.cxx
@@ -531,7 +531,6 @@ int main(int argc, char* argv[])
 	    
 	    //Apply cut value
 	    muonTrigger.emplace_back(k); //Take its index
-	    std::cout<<muonTrigger<<std::endl;
 		  
 	    for (Int_t j{0}; j<muonTrigger.size(); j++){
 		
