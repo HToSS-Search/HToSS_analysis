47456fe417154c13add2d0f3b6e7a0f8c55b0182
davidcarbonis <alexander.david.morton@cern.ch>
2020-11-06 17:50:15 +0100

    WIPush

--- src/histogramPlotter.cpp
+++ src/histogramPlotter.cpp
@@ -310,6 +310,7 @@ void HistogramPlotter::makePlot(std::map<std::string, TH1D*> plotMap,
             plotMap[*plot_iter]->SetFillStyle(4000);
             plotMap[*plot_iter]->SetLineColor(dsetMap_[*plot_iter].colour);
             plotMap[*plot_iter]->SetStats(false);
+            plotMap[*plot_iter]->SetTitle("");
             if ( emptyOverlay ) emptyOverlay = false;
             continue;
        	}
