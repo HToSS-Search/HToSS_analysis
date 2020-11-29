7a2d5c53cef1ba6e74e21c6b4f12f448a4703730
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2020-11-29 18:58:18 +0100

    Update simpleAnalysis.cxx

--- src/simpleAnalysis.cxx
+++ src/simpleAnalysis.cxx
@@ -764,14 +764,14 @@ int main(int argc, char* argv[])
                   if(event.packedCandsPseudoTrkPt[k]>pionpt1){
                     pionpt2=pionpt1;
                     pionpt1=event.packedCandsPseudoTrkPt[k];
-                    pionIndex.emplace_back(k);
+                  //  pionIndex.emplace_back(k);
                   }
                   else if(event.packedCandsPseudoTrkPt[k]>pionpt2){
                          pionpt2=event.packedCandsPseudoTrkPt[k];
-                         pionIndex.emplace_back(k);
+                     //    pionIndex.emplace_back(k);
                   }
                 }
-                std::cout<<"index pion "<<pionIndex[k]<<std::endl;
+               // std::cout<<"index pion "<<pionIndex[k]<<std::endl;
                 if(std::abs(packedId)==13 && event.packedCandsPseudoTrkPt[k]>5){//Selection of muons
                   muonIndex.emplace_back(k);
                   if(event.packedCandsPseudoTrkPt[k]>mupt1){
