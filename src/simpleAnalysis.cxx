aa517ca189df8591ed153bdae15486efc1d85c14
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2020-11-30 17:08:09 +0100

    Update simpleAnalysis.cxx

--- src/simpleAnalysis.cxx
+++ src/simpleAnalysis.cxx
@@ -297,7 +297,7 @@ int main(int argc, char* argv[])
     std::vector<int> nrofPion;
     
     Float_t genpt1=0; Float_t genpt2=0;
-    //std::cout << "idx\t | ID\t stat\t | Mo\t Da1\t Da2\t | pt\t eta\t phi\t m" << std::endl;
+    std::cout << "idx\t | ID\t stat\t | Mo\t Da1\t Da2\t | pt\t eta\t phi\t m" << std::endl;
          
        for (Int_t k{0}; k < event.nGenPar; k++) {
      
