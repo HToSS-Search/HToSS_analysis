2cf50cdfe104bd3dc6178a7e6973b17972453c0e
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2021-03-08 20:23:16 +0100

    Update refitvsnotmuon.C

--- scripts/refitvsnotmuon.C
+++ scripts/refitvsnotmuon.C
@@ -10,7 +10,7 @@
  #include "TLatex.h"
  #include "TPRegexp.h"
 
-  void refitvsnotMacro() {  
+  void refitvsnotmuon() {  
   
      TFile inFile0("ggH_MS2_ctau0.root"); 
      TH1F* h_refit12InvMass_ctau0    = (TH1F*)inFile0.Get("h_Rrefit12InvMass");
@@ -116,6 +116,6 @@
      canv1->Draw(); // draw canvas!
 
      // save canvas with drawn histogram
-     canv1->SaveAs("h_comparerefit.gif"); // .pdf and other formats work too! 
+     canv1->SaveAs("h_comparerefitmuon.gif"); // .pdf and other formats work too! 
    
   }
