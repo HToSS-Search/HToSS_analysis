6a9691ca6c3c79b7c62aab81e38874544947456e
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2021-03-09 00:30:26 +0100

    Update refitvsnotpion.C

--- scripts/refitvsnotpion.C
+++ scripts/refitvsnotpion.C
@@ -34,7 +34,7 @@
      canv1->SetLineColor(0);
      canv1->SetFillColor(0);
    
-     h_refit12InvMass_ctau0->SetTitle("Dimuon invariant mass with(out) refit");
+     h_refit12InvMass_ctau0->SetTitle("Dihadron (pion) invariant mass with(out) refit");
      h_refit12InvMass_ctau0->GetYaxis()->SetRangeUser(1.0, 65000.); 
      h_refit12InvMass_ctau0->GetYaxis()->SetTitle("Events");
      h_refit12InvMass_ctau0->GetXaxis()->SetRangeUser(0.0, 5.); 
