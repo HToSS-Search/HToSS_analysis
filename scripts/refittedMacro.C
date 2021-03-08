e0ad2a531b7d16247dc46f83690abf84a02852f3
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2021-03-09 00:49:43 +0100

    Update refittedMacro.C

--- scripts/refittedMacro.C
+++ scripts/refittedMacro.C
@@ -13,14 +13,14 @@
   void refittedMacro() {  
   
      TFile inFile0("ggH_MS2_ctau0.root"); 
-     TH1F* h_refit12InvMass_ctau0    = (TH1F*)inFile0.Get("h_refit12InvMass");
+     TH1F* h_refit12InvMass_ctau0    = (TH1F*)inFile0.Get("h_pionre12InvMass");
      
      
      TFile inFile10("ggH_MS2_ctau10.root"); 
-     TH1F* h_refit12InvMass_ctau10    = (TH1F*)inFile10.Get("h_refit12InvMass");
+     TH1F* h_refit12InvMass_ctau10    = (TH1F*)inFile10.Get("h_pionre12InvMass");
      
      TFile inFileQCD("ggH_MC_QCD.root"); 
-     TH1F* h_refit12InvMass_QCD    = (TH1F*)inFileQCD.Get("h_refit12InvMass");
+     TH1F* h_refit12InvMass_QCD    = (TH1F*)inFileQCD.Get("h_pionre12InvMass");
     
    
    
@@ -29,27 +29,27 @@
      canv1->SetLineColor(0);
      canv1->SetFillColor(0);
    
-     h_refit12InvMass_ctau0->SetTitle("Refitted dimuon invariant mass");
+     h_refit12InvMass_ctau0->SetTitle("Refitted dihadron (pion) invariant mass");
      h_refit12InvMass_ctau0->GetYaxis()->SetRangeUser(1., 100000.); 
      h_refit12InvMass_ctau0->GetYaxis()->SetTitle("Events");
-     h_refit12InvMass_ctau0->GetXaxis()->SetRangeUser(0.0, 5.); 
-     h_refit12InvMass_ctau0->GetXaxis()->SetTitle("m_{#mu#mu} (GeV/c^{2})");
+     h_refit12InvMass_ctau0->GetXaxis()->SetRangeUser(0.0, 3.); 
+     h_refit12InvMass_ctau0->GetXaxis()->SetTitle("m_{dihadron} (GeV/c^{2})");
      h_refit12InvMass_ctau0->SetMarkerColor(2);
      h_refit12InvMass_ctau0->SetLineColor(2);
      h_refit12InvMass_ctau0->Draw("HIST");
  
      h_refit12InvMass_ctau10->GetYaxis()->SetTitle("Events");
      h_refit12InvMass_ctau10->GetYaxis()->SetRangeUser(1., 100000.); 
-     h_refit12InvMass_ctau10->GetXaxis()->SetRangeUser(0.0, 5.); 
-     h_refit12InvMass_ctau10->GetXaxis()->SetTitle("m_{#mu#mu} (GeV/c^{2})");
+     h_refit12InvMass_ctau10->GetXaxis()->SetRangeUser(0.0, 3.); 
+     h_refit12InvMass_ctau10->GetXaxis()->SetTitle("m_{dihadron} (GeV/c^{2})");
      h_refit12InvMass_ctau10->SetMarkerColor(4);
      h_refit12InvMass_ctau10->SetLineColor(4);
      h_refit12InvMass_ctau10->Draw("HIST same");
    
      h_refit12InvMass_QCD->GetYaxis()->SetTitle("Events");
-     h_refit12InvMass_QCD->GetXaxis()->SetRangeUser(0., 5.); 
+     h_refit12InvMass_QCD->GetXaxis()->SetRangeUser(0., 3.); 
      h_refit12InvMass_QCD->GetYaxis()->SetRangeUser(1.0, 100000.); 
-     h_refit12InvMass_QCD->GetXaxis()->SetTitle("m_{#mu#mu} (GeV/c^{2})");
+     h_refit12InvMass_QCD->GetXaxis()->SetTitle("m_{dihadron} (GeV/c^{2})");
      h_refit12InvMass_QCD->SetMarkerColor(6);
      h_refit12InvMass_QCD->SetLineColor(6);
      h_refit12InvMass_QCD->Draw("HIST same");
