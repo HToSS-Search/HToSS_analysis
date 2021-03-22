64abb6b870d859540c063d7ab6848165e82dad22
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2021-03-22 18:45:05 +0100

    Update refitvsnotmuon.C

--- scripts/refitvsnotmuon.C
+++ scripts/refitvsnotmuon.C
@@ -144,7 +144,7 @@
      canv2->SetFillColor(0);
    
      h_scalescalarInvMass_QCD->SetTitle("Dimuon invariant mass, wider Higgs window");
-     h_scalescalarInvMass_QCD->GetYaxis()->SetRangeUser(1.0, 100000.); 
+     h_scalescalarInvMass_QCD->GetYaxis()->SetRangeUser(1.0, 65000.); 
      h_scalescalarInvMass_QCD->GetYaxis()->SetTitle("Events");
      h_scalescalarInvMass_QCD->GetXaxis()->SetRangeUser(0.5, 4.); 
      h_scalescalarInvMass_QCD->GetXaxis()->SetTitle("m_{#mu#mu} (GeV/c^{2})");
@@ -166,13 +166,13 @@
      h_P20scalarInvMass_ctau10->SetLineColor(4);
      h_P20scalarInvMass_ctau10->Draw("HIST same");
    
-   
+   /*
      h_PrescalescalarInvMass_QCD->GetYaxis()->SetTitle("Events");
      h_PrescalescalarInvMass_QCD->GetXaxis()->SetRangeUser(0.5, 4.); 
      h_PrescalescalarInvMass_QCD->GetXaxis()->SetTitle("m_{#mu#mu} (GeV/c^{2})");
      h_PrescalescalarInvMass_QCD->SetMarkerColor(52);
      h_PrescalescalarInvMass_QCD->SetLineColor(52);
-     h_PrescalescalarInvMass_QCD->Draw("HIST");
+     h_PrescalescalarInvMass_QCD->Draw("HIST");*/
    
      TLegend *legend2 = new TLegend(0.7,0.7,0.9,0.9);
      legend2->SetBorderSize(1);
