a65c49f89e4a9da659441fca2d1f29d2efc22035
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2021-03-22 16:39:25 +0100

    Update refitvsnotmuon.C

--- scripts/refitvsnotmuon.C
+++ scripts/refitvsnotmuon.C
@@ -158,6 +158,15 @@
      h_P20scalarInvMass_ctau10->SetLineColor(4);
      h_P20scalarInvMass_ctau10->Draw("HIST same");
    
+     h_scalescalarInvMass_QCD->Scale(corr);
+    // h_refit12InvMass_ctau0->GetYaxis()->SetRangeUser(1.0, 65000.); 
+     h_scalescalarInvMass_QCD->GetYaxis()->SetTitle("Events");
+     h_scalescalarInvMass_QCD->GetXaxis()->SetRangeUser(0.5, 4.); 
+     h_scalescalarInvMass_QCD->GetXaxis()->SetTitle("m_{#mu#mu} (GeV/c^{2})");
+     h_scalescalarInvMass_QCD->SetMarkerColor(52);
+     h_scalescalarInvMass_QCD->SetLineColor(52);
+     h_scalescalarInvMass_QCD->Draw("HIST");
+   
      TLegend *legend2 = new TLegend(0.7,0.7,0.9,0.9);
      legend2->SetBorderSize(1);
      TLegendEntry *legendb = legend2->AddEntry("h_scalescalarInvMass_QCD","QCD","L");
@@ -171,6 +180,10 @@
      legendb=legend2->AddEntry("h_P20scalarInvMass_ctau10","c_{#tau}=10mm","L");
      legendb->SetLineColor(4);
      legendb->SetMarkerColor(4);
+     legendb->SetMarkerSize(2);
+     legendb=legend2->AddEntry("h_scalescalarInvMass_QCD","Normalised QCD","L");
+     legendb->SetLineColor(52);
+     legendb->SetMarkerColor(52);
      legendb->SetMarkerSize(2);
      legend2->Draw();
    
