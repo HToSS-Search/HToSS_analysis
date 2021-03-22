d9fe5786992fe708ade7506514fd51e0f65f4557
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2021-03-22 16:47:09 +0100

    Update refitvsnotmuon.C

--- scripts/refitvsnotmuon.C
+++ scripts/refitvsnotmuon.C
@@ -34,9 +34,8 @@
      Int_t n20entries=h_P20scalarInvMass_ctau0->GetEntries();
      Float_t corr=nentries/n20entries;
    
-     TH1F* h_PrescalescalarInvMass_QCD    = (TH1F*)inFileQCD.Get("h_P20scalarInvMass"*corr);
      TH1F* h_scalescalarInvMass_QCD    = (TH1F*)inFileQCD.Get("h_P20scalarInvMass");
-     
+     TH1F* h_PrescalescalarInvMass_QCD=(TH1F*)inFileQCD.Get("h_P20scalarInvMass")*corr;
    
      TCanvas* canv1 = new TCanvas ("canv1", "canv1", 50, 50, 800, 600); // Canvas to draw histogram on
      gStyle->SetOptStat(0);
@@ -158,14 +157,14 @@
      h_P20scalarInvMass_ctau10->SetLineColor(4);
      h_P20scalarInvMass_ctau10->Draw("HIST same");
    
-     h_scalescalarInvMass_QCD->Scale(corr);
+     h_PrescalescalarInvMass_QCD->Scale(corr);
     // h_refit12InvMass_ctau0->GetYaxis()->SetRangeUser(1.0, 65000.); 
-     h_scalescalarInvMass_QCD->GetYaxis()->SetTitle("Events");
-     h_scalescalarInvMass_QCD->GetXaxis()->SetRangeUser(0.5, 4.); 
-     h_scalescalarInvMass_QCD->GetXaxis()->SetTitle("m_{#mu#mu} (GeV/c^{2})");
-     h_scalescalarInvMass_QCD->SetMarkerColor(52);
-     h_scalescalarInvMass_QCD->SetLineColor(52);
-     h_scalescalarInvMass_QCD->Draw("HIST");
+     h_PrescalescalarInvMass_QCD->GetYaxis()->SetTitle("Events");
+     h_PrescalescalarInvMass_QCD->GetXaxis()->SetRangeUser(0.5, 4.); 
+     h_PrescalescalarInvMass_QCD->GetXaxis()->SetTitle("m_{#mu#mu} (GeV/c^{2})");
+     h_PrescalescalarInvMass_QCD->SetMarkerColor(52);
+     h_PrescalescalarInvMass_QCD->SetLineColor(52);
+     h_PrescalescalarInvMass_QCD->Draw("HIST");
    
      TLegend *legend2 = new TLegend(0.7,0.7,0.9,0.9);
      legend2->SetBorderSize(1);
@@ -181,7 +180,7 @@
      legendb->SetLineColor(4);
      legendb->SetMarkerColor(4);
      legendb->SetMarkerSize(2);
-     legendb=legend2->AddEntry("h_scalescalarInvMass_QCD","Normalised QCD","L");
+     legendb=legend2->AddEntry("h_PrescalescalarInvMass_QCD","Normalised QCD","L");
      legendb->SetLineColor(52);
      legendb->SetMarkerColor(52);
      legendb->SetMarkerSize(2);
