2dbe3300ae38f79692898f3f001dee2f7c5578bf
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2021-03-22 23:59:05 +0100

    Update refitvsnotmuon.C

--- scripts/refitvsnotmuon.C
+++ scripts/refitvsnotmuon.C
@@ -13,19 +13,19 @@
 
   void refitvsnotmuon() {  
   
-     TFile inFile0("ggH_MS2_ctau0.root"); 
+     TFile inFile0("ggH_MS2_ctau0_kaon.root"); 
      TH1F* h_refit12InvMass_ctau0    = (TH1F*)inFile0.Get("h_Rrefit12InvMass");
      TH1F* h_PscalarInvMass_ctau0    = (TH1F*)inFile0.Get("h_PscalarInvMass");
      TH1F* h_P20scalarInvMass_ctau0    = (TH1F*)inFile0.Get("h_P20higgsInvMass");
     // TH1F* h_PantiscalarInvMass_ctau0    = (TH1F*)inFile0.Get("h_PantiscalarInvMass");
 
-     TFile inFile10("ggH_MS2_ctau10.root"); 
+     TFile inFile10("ggH_MS2_ctau10_kaon.root"); 
      TH1F* h_refit12InvMass_ctau10    = (TH1F*)inFile10.Get("h_Rrefit12InvMass");
      TH1F* h_PscalarInvMass_ctau10    = (TH1F*)inFile10.Get("h_PscalarInvMass");
      TH1F* h_P20scalarInvMass_ctau10    = (TH1F*)inFile10.Get("h_P20higgsInvMass");
    //  TH1F* h_PantiscalarInvMass_ctau10    = (TH1F*)inFile10.Get("h_PantiscalarInvMass");
 
-     TFile inFileQCD("ggH_MC_QCD.root"); 
+     TFile inFileQCD("ggH_MC_QCD_kaon.root"); 
      TH1F* h_refit12InvMass_QCD    = (TH1F*)inFileQCD.Get("h_Rrefit12InvMass");
      TH1F* h_PscalarInvMass_QCD    = (TH1F*)inFileQCD.Get("h_PscalarInvMass");
    //  TH1F* h_P20scalarInvMass_QCD    = (TH1F*)inFileQCD.Get("h_P20scalarInvMass");
