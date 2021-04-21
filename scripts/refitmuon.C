95cfc6fac0acc3206da27a82e995e6c39664ce00
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2021-04-21 11:50:44 +0200

    Update refitmuon.C

--- scripts/refitmuon.C
+++ scripts/refitmuon.C
@@ -68,14 +68,14 @@
    
      
      h_refit12InvMass_ctau0->GetYaxis()->SetTitle("Events");
-     h_refit12InvMass_ctau0->GetXaxis()->SetRangeUser(0,4); 
+     h_refit12InvMass_ctau0->GetXaxis()->SetRangeUser(1,3); 
      h_refit12InvMass_ctau0->GetXaxis()->SetTitle("m_{dimuon} (GeV/c^{2})");
      h_refit12InvMass_ctau0->SetMarkerColor(4);
      h_refit12InvMass_ctau0->SetLineColor(4);
      h_refit12InvMass_ctau0->Draw("HIST same");
      
      h_refit12InvMass_ctau10->GetYaxis()->SetTitle("Events");
-     h_refit12InvMass_ctau10->GetXaxis()->SetRangeUser(0,4); 
+     h_refit12InvMass_ctau10->GetXaxis()->SetRangeUser(1,3); 
      h_refit12InvMass_ctau10->GetXaxis()->SetTitle("m_{dimuon} (GeV/c^{2})");
      h_refit12InvMass_ctau10->SetMarkerColor(3);
      h_refit12InvMass_ctau10->SetLineColor(3);
@@ -83,14 +83,14 @@
      std::cout<<"Refit "<<h_refit12InvMass_ctau0->Integral(0,501)<<std::endl;
 
      h_refit12InvMass_QCD->GetYaxis()->SetTitle("Events");
-     h_refit12InvMass_QCD->GetXaxis()->SetRangeUser(0,4); 
+     h_refit12InvMass_QCD->GetXaxis()->SetRangeUser(1,3); 
      h_refit12InvMass_QCD->GetXaxis()->SetTitle("m_{dimuon} (GeV/c^{2})");
      h_refit12InvMass_QCD->SetMarkerColor(6);
      h_refit12InvMass_QCD->SetLineColor(6);
      h_refit12InvMass_QCD->Draw("HIST same");
      
      h_refit12InvMass_data->GetYaxis()->SetTitle("Events");
-     h_refit12InvMass_data->GetXaxis()->SetRangeUser(0,4); 
+     h_refit12InvMass_data->GetXaxis()->SetRangeUser(1,3); 
      h_refit12InvMass_data->GetXaxis()->SetTitle("m_{dimuon} (GeV/c^{2})");
      h_refit12InvMass_data->SetMarkerColor(94);
      h_refit12InvMass_data->SetLineColor(94);
