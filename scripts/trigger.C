3590a9a54d94d729d1ce922d8e08504751e70445
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2021-05-02 11:38:32 +0200

    Update trigger.C

--- scripts/trigger.C
+++ scripts/trigger.C
@@ -15,33 +15,20 @@
      TFile inFile0("ggH_MS2_ctau0.root"); // read in file. Exercise: can this be done through a command line arguement? That way you don't have to edit code for each new histogram ...
      TH1F* h_pt_ctau0    = (TH1F*)inFile0.Get("h_genParScalarMuonPtL");
      TH1F* h_ptcut_ctau0    = (TH1F*)inFile0.Get("h_genParScalarMuonCutPtSL;1");
-    // TH1F* h_ptdiv_ctau0    = (TH1F*)inFile0.Get("h_genParScalarMuonCutPtSL;2");
-    // TH1F* h_KIsoSum1_ctau0    = (TH1F*)inFile0.Get("h_KIsoSum1");
-    // TH1F* h_KIsoSum2_ctau0    = (TH1F*)inFile0.Get("h_KIsoSum2");
-    // TH1F* h_MuonIsoSum1_ctau0    = (TH1F*)inFile0.Get("h_MuonIsoSum1");
-    // TH1F* h_MuonIsoSum2_ctau0    = (TH1F*)inFile0.Get("h_MuonIsoSum2"); 
-     
+   
      TFile inFile10("ggH_MS2_ctau10.root"); 
      TH1F* h_pt_ctau10    = (TH1F*)inFile10.Get("h_genParScalarMuonPtL");
      TH1F* h_ptcut_ctau10    = (TH1F*)inFile10.Get("h_genParScalarMuonCutPtSL;1");
-   //  TH1F* h_ptdiv_ctau10    = (TH1F*)inFile10.Get("h_genParScalarMuonCutPtSL;2");
-    // TH1F* h_KIsoSum1_ctau10    = (TH1F*)inFile10.Get("h_KIsoSum1");
-    // TH1F* h_KIsoSum2_ctau10    = (TH1F*)inFile10.Get("h_KIsoSum2");
-//TH1F* h_MuonIsoSum1_ctau10    = (TH1F*)inFile10.Get("h_MuonIsoSum1");
-   //  TH1F* h_MuonIsoSum2_ctau10    = (TH1F*)inFile10.Get("h_MuonIsoSum2");
+   
    
      TFile inFile100("ggH_MS2_ctau100.root"); 
      TH1F* h_pt_ctau100    = (TH1F*)inFile100.Get("h_genParScalarMuonPtL");
      TH1F* h_ptcut_ctau100    = (TH1F*)inFile100.Get("h_genParScalarMuonCutPtSL;1");
-    // TH1F* h_ptdiv_ctau100    = (TH1F*)inFile100.Get("h_genParScalarMuonCutPtSL;2");
-   //  TH1F* h_KIsoSum1_ctau10    = (TH1F*)inFile10.Get("h_KIsoSum1");
-   //  TH1F* h_KIsoSum2_ctau10    = (TH1F*)inFile10.Get("h_KIsoSum2");
-   //  TH1F* h_MuonIsoSum1_ctau10    = (TH1F*)inFile10.Get("h_MuonIsoSum1");
-   //  TH1F* h_MuonIsoSum2_ctau10    = (TH1F*)inFile10.Get("h_MuonIsoSum2");
+   
     
-    TFile inFile100O("ggH_MS2_ctau1000.root"); 
+    /*TFile inFile100O("ggH_MS2_ctau1000.root"); 
      TH1F* h_pt_ctau1000    = (TH1F*)inFile1000.Get("h_genParScalarMuonPtL");
-     TH1F* h_ptcut_ctau1000    = (TH1F*)inFile1000.Get("h_genParScalarMuonCutPtSL;1");
+     TH1F* h_ptcut_ctau1000    = (TH1F*)inFile1000.Get("h_genParScalarMuonCutPtSL;1");*/
    
    
      TCanvas* canv1 = new TCanvas ("canv1", "canv1", 50, 50, 800, 600); // Canvas to draw histogram on
@@ -50,7 +37,6 @@
      canv1->SetLineColor(0);
      canv1->SetFillColor(0);
    
-     //h_pt_ctau0->SetTitle("Leading pion, relative isolation");
      h_pt_ctau10->GetYaxis()->SetTitle("Events");
      h_pt_ctau10->GetXaxis()->SetRangeUser(0.0,100); 
      h_pt_ctau10->GetXaxis()->SetTitle("p_{T} (GeV)");
@@ -69,7 +55,7 @@
    
      TLegend *legend1 = new TLegend(0.7,0.7,0.9,0.9);
      legend1->SetBorderSize(1);
-     TLegendEntry *legenda = legend1->AddEntry("h_pt_ctau10","c_{#tau}=10mm","L");
+     TLegendEntry *legenda = legend1->AddEntry("h_pt_ctau10","c_{#tau}=10mm, no trigger","L");
      legenda->SetLineColor(4);
      legenda->SetMarkerColor(4);
      legenda->SetMarkerSize(2);
