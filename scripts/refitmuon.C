bdf0c364affa942c4f6d97d262160411fd7c9ec2
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2021-05-03 11:54:15 +0200

    Update refitmuon.C

--- scripts/refitmuon.C
+++ scripts/refitmuon.C
@@ -71,7 +71,7 @@
      h_PscalarInvMass_QCD->Draw("HIST same");
    
      h_PscalarInvMass_data->GetYaxis()->SetTitle("Events");
-     h_PscalarInvMass_data->GetXaxis()->SetRangeUser(120,130); 
+     h_PscalarInvMass_data->GetXaxis()->SetRangeUser(100,150); 
      h_PscalarInvMass_data->GetXaxis()->SetTitle("m_{Higgs} (GeV/c^{2})");
      h_PscalarInvMass_data->SetMarkerColor(94);
      h_PscalarInvMass_data->SetLineColor(94);
