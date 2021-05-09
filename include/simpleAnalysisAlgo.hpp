bef336776bd313149645ce79f9e269be8559d442
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2021-05-09 17:25:24 +0200

    Update simpleAnalysisAlgo.hpp

--- include/simpleAnalysisAlgo.hpp
+++ include/simpleAnalysisAlgo.hpp
@@ -197,6 +197,7 @@ class SimpleAnalysis
     TH1F* h_KantiscalarInvMass;
     TH1F* h_KscalarInvMass;
     TH1F* h_KhiggsInvMass;
+    TH1F* h_K20higgsInvMass;
     TH1F* h_KhiggsRInvMass;
     TH1F* h_KhiggsDeltaR;
     TH1F* h_kNentries1; TH1F* h_kNentries2; TH1F* h_kNentries3; TH1F* h_kNentries4;
