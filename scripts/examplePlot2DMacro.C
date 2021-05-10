c028d82718f90cc1a58f5a1873e71e8508db6138
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2021-05-10 21:47:17 +0200

    Update examplePlot2DMacro.C

--- scripts/examplePlot2DMacro.C
+++ scripts/examplePlot2DMacro.C
@@ -19,9 +19,9 @@
      gPad->SetLogz(); // set log scale for y-axis on for objects drawn on the canvas from this point on
 
      h_VertexPosXY->GetXaxis()->SetTitle("Vertex position x (mm)"); // set a title for the x-axis
-     h_VertexPosXY->GetXaxis()->SetRangeUser(0.0, 50.); // change the x-axis range to 0-200
+     h_VertexPosXY->GetXaxis()->SetRangeUser(-100., 100.); // change the x-axis range to 0-200
      h_VertexPosXY->GetYaxis()->SetTitle("Vertex Position y (mm)"); 
-     h_VertexPosXY->GetYaxis()->SetRangeUser(0.0, 50.); //
+     h_VertexPosXY->GetYaxis()->SetRangeUser(-100., 100.); //
  
      h_VertexPosXY->Draw("colz"); // draw histo on canvas
      canv2->Draw(); // draw canvas!
