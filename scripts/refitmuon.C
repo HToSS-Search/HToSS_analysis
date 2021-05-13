c27b9335183758623440ef18f5c020a010af69eb
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2021-05-13 22:34:36 +0200

    Update refitmuon.C

--- scripts/refitmuon.C
+++ scripts/refitmuon.C
@@ -14,7 +14,7 @@
   
      TFile inFile0("ggH_MS2_ctau0.root"); 
      TH1F* h_refit12InvMass_ctau0    = (TH1F*)inFile0.Get("h_Rrefit12InvMass");
-     TH1F* h_PscalarInvMass_ctau0    = (TH1F*)inFile0.Get("h_PhiggsRInvMass");
+     TH1F* h_PscalarInvMass_ctau0    = (TH1F*)inFile0.Get("h_KantiscalarInvMass");
 
      TFile inFile10("ggH_MS2_ctau10.root"); 
      TH1F* h_refit12InvMass_ctau10    = (TH1F*)inFile10.Get("h_Rrefit12InvMass");
