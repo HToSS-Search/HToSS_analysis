a5f53da35b910bde45a21023a1061ed85353dca0
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2021-05-18 08:51:19 +0200

    Update refitmuon.C

--- scripts/refitmuon.C
+++ scripts/refitmuon.C
@@ -15,22 +15,22 @@
   
      TFile inFile0("ggH_MS2_ctau0.root"); 
      TH1F* h_refit12InvMass_ctau0    = (TH1F*)inFile0.Get("h_Rpionre12InvMass");
-     TH1F* h_PscalarInvMass_ctau0    = (TH1F*)inFile0.Get("h_Rpionre12InvMass");
+     TH1F* h_PscalarInvMass_ctau0    = (TH1F*)inFile0.Get("h_PantiscalarInvMass");
      
      TFile inFile10("ggH_MS2_ctau10.root"); 
      TH1F* h_refit12InvMass_ctau10    = (TH1F*)inFile10.Get("h_Rpionre12InvMass");
-     TH1F* h_PscalarInvMass_ctau10    = (TH1F*)inFile10.Get("h_Rpionre12InvMass");
+     TH1F* h_PscalarInvMass_ctau10    = (TH1F*)inFile10.Get("h_PantiscalarInvMass");
 
      TFile inFileQCD("ggH_MC_QCD_2GeV.root"); 
      TH1F* h_refit12InvMass_QCD    = (TH1F*)inFileQCD.Get("h_Rpionre12InvMass");
-     TH1F* h_PscalarInvMass_QCD    = (TH1F*)inFileQCD.Get("h_Rpionre12InvMass");
-     TH1F* h_P20scalarInvMass_QCD    = (TH1F*)inFileQCD.Get("h_widepionRInvMass");
+     TH1F* h_PscalarInvMass_QCD    = (TH1F*)inFileQCD.Get("h_PantiscalarInvMass");
+     TH1F* h_P20scalarInvMass_QCD    = (TH1F*)inFileQCD.Get("h_P20antiscalarInvMass");
      //TObject* corrsmall = inFileQCD.Get("QCD_Pantiscalar");
      //TObject* corrwide = inFileQCD.Get("QCD_20Pantiscalar");
       
      TFile inFileData("dataRunD_2GeV.root"); 
      TH1F* h_refit12InvMass_data    = (TH1F*)inFileData.Get("h_Rpionre12InvMass");
-     TH1F* h_PscalarInvMass_data    = (TH1F*)inFileData.Get("h_Rpionre12InvMass");
+     TH1F* h_PscalarInvMass_data    = (TH1F*)inFileData.Get("h_PantiscalarInvMass");
 
    
      TCanvas* canv1 = new TCanvas ("canv1", "canv1", 50, 50, 800, 600); // Canvas to draw histogram on
