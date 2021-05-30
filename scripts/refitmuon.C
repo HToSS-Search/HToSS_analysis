225f6e48965448c460b519edd675c74a860897c5
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2021-05-30 17:22:57 +0200

    Update refitmuon.C

--- scripts/refitmuon.C
+++ scripts/refitmuon.C
@@ -15,20 +15,20 @@
   
      TFile inFile0("ggH_MS2_ctau0.root"); 
      TH1F* h_refit12InvMass_ctau0    = (TH1F*)inFile0.Get("h_Rpionre12InvMass");
-     TH1F* h_PscalarInvMass_ctau0    = (TH1F*)inFile0.Get("h_KhiggsRInvMass");
+     TH1F* h_PscalarInvMass_ctau0    = (TH1F*)inFile0.Get("h_KhiggsInvMass");
      
      TFile inFile10("ggH_MS2_ctau10.root"); 
      TH1F* h_refit12InvMass_ctau10    = (TH1F*)inFile10.Get("h_Rpionre12InvMass");
-     TH1F* h_PscalarInvMass_ctau10    = (TH1F*)inFile10.Get("h_KhiggsRInvMass");
+     TH1F* h_PscalarInvMass_ctau10    = (TH1F*)inFile10.Get("h_KhiggsInvMass");
 
      TFile inFileQCD("ggH_MC_QCD_2GeV.root"); 
      TH1F* h_refit12InvMass_QCD    = (TH1F*)inFileQCD.Get("h_Rpionre12InvMass");
-     TH1F* h_PscalarInvMass_QCD    = (TH1F*)inFileQCD.Get("h_KhiggsRInvMass");
+     TH1F* h_PscalarInvMass_QCD    = (TH1F*)inFileQCD.Get("h_KhiggsInvMass");
      TH1F* h_P20scalarInvMass_QCD    = (TH1F*)inFileQCD.Get("h_KhiggsR20InvMass");
       
      TFile inFileData("dataRunD_2GeV.root"); 
      TH1F* h_refit12InvMass_data    = (TH1F*)inFileData.Get("h_Rpionre12InvMass");
-     TH1F* h_PscalarInvMass_data    = (TH1F*)inFileData.Get("h_KhiggsRInvMass");
+     TH1F* h_PscalarInvMass_data    = (TH1F*)inFileData.Get("h_KhiggsInvMass");
 
    
      TCanvas* canv1 = new TCanvas ("canv1", "canv1", 50, 50, 800, 600); // Canvas to draw histogram on
