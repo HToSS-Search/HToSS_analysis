a66d090b46ccc90942a5f4a57faf033f011feb41
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2021-06-19 11:13:24 +0200

    Update Limits_2GeV.C

--- scripts/Limits_2GeV.C
+++ scripts/Limits_2GeV.C
@@ -1,8 +1,7 @@
-#include "TFile.h"
-
+ #include "TFile.h"
  #include "TCanvas.h"
  #include "TROOT.h"
-
+ #include "TGraphPainter.h"
  #include "TLegend.h"
  #include "TLegendEntry.h"
  #include "TStyle.h"
@@ -13,16 +12,18 @@
   void Limits_2GeV() {  
   
      TCanvas* canv1 = new TCanvas ("canv1", "canv1", 50, 50, 800, 600); // Canvas to draw histogram on
+     canv1->SetGrid();
+   
+     double x1[] = {0, 1, 10};
    
      //observed
-     double x1[] = {0, 1, 10};
      double y1[] = {0.0369502, 0.0479211, 0.477744}; //value
      double ex1[] = {0., 0., 0.};
      double ey1[] = {0.00708996, 0.00871479, 0.00552663 }; //error
      auto obs = new TGraphErrors(3, x1, y1, ex1, ey1);
      obs->SetMarkerColor(4); //blue
      obs->SetMarkerStyle(21); //square
-     obs->Draw("ALP");
+     obs->Draw("alp");
    
      //expected (median)
      double y2[] = {0.0290654, 0.0383966, 0.383998}; //value
@@ -31,7 +32,7 @@
      auto exp = new TGraphErrors(3, x1, y2, ex2, ey2);
      exp->SetMarkerColor(1); //black
      exp->SetMarkerStyle(21);
-     exp->Draw("AP same");
+     exp->Draw("ap");
      
      //2 sigma band
      double y3[] = {0.03863480, 0.04905365, 0.49274297}; 
@@ -40,18 +41,20 @@
      auto sigma2 = new TGraphErrors(3, x1, y3, ex3, ey3);
      sigma2->SetFillColor(5);
      sigma2->SetFillStyle(3001);
-     sigma2->Draw("A3");
+     sigma2->Draw("a3");
      
      //1 sigma band
-     /*double y4[] = {0.03444700,0.04362106,0.41502756};
+     double y4[] = {0.03444700,0.04362106,0.41502756};
      double ex4[] = {0., 0., 0.};
      double ey4[] = {0.01427847,0.01799699,0.14057155};
      auto sigma1 = new TGraphErrors(3, x1, y4, ex4, ey4);
      sigma1->SetFillColor(8);
      sigma1->SetFillStyle(3001);
-     sigma1->Draw("A3 same");*/
+     sigma1->GetXaxis()->SetTitle("c#tau (mm)");
+     sigma1->GetYaxis()->SetTitle("Signal strength (r)");
+     sigma1->Draw("a3");
    
-   
+     
    
      canv1->Modified();
      canv1->cd();
