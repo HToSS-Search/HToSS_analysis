7dbc40da35dce6363ca1712be7224f28e93e5115
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2021-06-19 11:21:56 +0200

    Update Limits_2GeV.C

--- scripts/Limits_2GeV.C
+++ scripts/Limits_2GeV.C
@@ -14,39 +14,39 @@
      TCanvas* canv1 = new TCanvas ("canv1", "canv1", 50, 50, 800, 600); // Canvas to draw histogram on
      canv1->SetGrid();
    
-     double x1[] = {0, 1, 10};
+     double x1[3] = {0, 1, 10};
    
      //observed
-     double y1[] = {0.0369502, 0.0479211, 0.477744}; //value
-     double ex1[] = {0., 0., 0.};
-     double ey1[] = {0.00708996, 0.00871479, 0.00552663 }; //error
+     double y1[3] = {0.0369502, 0.0479211, 0.477744}; //value
+     double ex1[3] = {0., 0., 0.};
+     double ey1[3] = {0.00708996, 0.00871479, 0.00552663 }; //error
      auto obs = new TGraphErrors(3, x1, y1, ex1, ey1);
      obs->SetMarkerColor(4); //blue
      obs->SetMarkerStyle(21); //square
      obs->Draw("alp ");
    
      //expected (median)
-     double y2[] = {0.0290654, 0.0383966, 0.383998}; //value
-     double ex2[] = {0., 0., 0.};
-     double ey2[] = {0.00549035, 0.000545785, 0.00417906}; //error
+     double y2[3] = {0.0290654, 0.0383966, 0.383998}; //value
+     double ex2[3] = {0., 0., 0.};
+     double ey2[3] = {0.00549035, 0.000545785, 0.00417906}; //error
      auto exp = new TGraphErrors(3, x1, y2, ex2, ey2);
      exp->SetMarkerColor(1); //black
      exp->SetMarkerStyle(21);
      exp->Draw("ap same");
      
      //2 sigma band
-     double y3[] = {0.03863480, 0.04905365, 0.49274297}; 
-     double ex3[] = {0.1, 0.2, 0.3};
-     double ey3[] = {0.02339717, 0.02965788, 0.27229374};
+     double y3[3] = {0.03863480, 0.04905365, 0.49274297}; 
+     double ex3[3] = {0., 0., 0.};
+     double ey3[3] = {0.02339717, 0.02965788, 0.27229374};
      auto sigma2 = new TGraphErrors(3, x1, y3, ex3, ey3);
      sigma2->SetFillColor(5);
      sigma2->SetFillStyle(3001);
      sigma2->Draw("a3 same");
      
      //1 sigma band
-     double y4[] = {0.03444700,0.04362106,0.41502756};
-     double ex4[] = {0., 0., 0.};
-     double ey4[] = {0.01427847,0.01799699,0.14057155};
+     double y4[3] = {0.03444700,0.04362106,0.41502756};
+     double ex4[3] = {0., 0., 0.};
+     double ey4[3] = {0.01427847,0.01799699,0.14057155};
      auto sigma1 = new TGraphErrors(3, x1, y4, ex4, ey4);
      sigma1->SetFillColor(8);
      sigma1->SetFillStyle(3001);
