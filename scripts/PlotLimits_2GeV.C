9ec79a5da0c30e345d32a02d93269be2e7951960
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2021-06-19 08:49:37 +0200

    Update PlotLimits_2GeV.C

--- scripts/PlotLimits_2GeV.C
+++ scripts/PlotLimits_2GeV.C
@@ -17,19 +17,42 @@
      canv1->SetLineColor(0);
      canv1->SetFillColor(0);
    
-     double x1[] = {0, 1, 10, 100};
-     double y1[] = {0.477744, 2, 4, 1};
-     double ex1[] = {0., 0., 0., 0.};
-     double ey1[] = {0.00552663, 0.005, 0.005, 0.005};
-     auto obs = new TGraphErrors(4, x1, y1, ex1, ey1);
-     obs->Draw("ap");
+     double x1[] = {0, 1, 10};
+     double y1[] = {0, 0.0479211, 0.477744}; //value
+     double ex1[] = {0., 0., 0.};
+     double ey1[] = {0., 0.00871479, 0.00552663 }; //error
+     auto obs = new TGraphErrors(3, x1, y1, ex1, ey1);
+     obs->SetMarkerColor(4); //blue
+     obs->SetMarkerStyle(21); //square
+     obs->Draw("ALP");
+   
     
-     /*double y2[] = {0.383998, 2, 4, 1};
-     double ex2[] = {0., 0., 0., 0.};
-     double ey2[] = {0.00417906, 0.005, 0.005, 0.005};
-     auto exp = new TGraphErrors(4, x1, y2, ex2, ey2);
-     exp->Draw("ap same");*/
+     double y2[] = {0, 0.0383966, 0.383998}; //value
+     double ex2[] = {0., 0., 0.};
+     double ey2[] = {0, 0.000545785, 0.00417906}; //error
+     auto exp = new TGraphErrors(3, x1, y2, ex2, ey2);
+     exp->SetMarkerColor(1); //black
+     exp->SetMarkerStyle(21);
+     exp->Draw("AP same");
     
+     
+     double x[] = {0, 1, 2};
+     double y[] = {0, 2, 4};
+     double ex[] = {0.1, 0.2, 0.3};
+     double ey[] = {1, 0.5, 1};
+     auto ge = new TGraphErrors(3, x, y, ex, ey);
+     up1->SetFillColor(8);
+     up1->SetFillStyle(3001);
+     up->Draw("A4");
+   
+     double x[] = {0, 1, 2};
+     double y[] = {0, 2, 4};
+     double ex[] = {0.1, 0.2, 0.3};
+     double ey[] = {1, 0.5, 1};
+     auto ge = new TGraphErrors(3, x, y, ex, ey);
+     ge->SetFillColor(5);
+     ge->SetFillStyle(3001);
+     ge->Draw("A4");
    
      canv1->Modified();
      canv1->cd();
