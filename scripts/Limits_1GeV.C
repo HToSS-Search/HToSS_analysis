dbddafca4f0c2a2efc38513e9a24d650e5d10eb5
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2021-06-19 16:35:48 +0200

    Create Limits_1GeV.C

---
+++ scripts/Limits_1GeV.C
@@ -0,0 +1,90 @@
+#include "TFile.h"
+ #include "TCanvas.h"
+ #include "TROOT.h"
+ #include "TGraphPainter.h"
+ #include "TLegend.h"
+ #include "TLegendEntry.h"
+ #include "TStyle.h"
+ #include "TString.h"
+ #include "TLatex.h"
+ #include "TPRegexp.h"
+ #include "TAttLine.h"
+
+  void Limits_1GeV() {  
+  
+     TCanvas* canv1 = new TCanvas ("canv1", "canv1", 50, 50, 800, 600); // Canvas to draw histogram on
+     canv1->SetGrid();
+     //gPad->SetLogx(1);
+   
+     double x1[3] = {0, 1, 10};//, 100};
+    
+     //2 sigma band
+     double y3[3] = {0.03863480, 0.04905365, 0.49274297};//,41.79250050}; 
+     double ex3[3] = {0., 0., 0.};//, 0.};
+     double ey3[3] = {0.02339717, 0.02965788, 0.27229374};//,24.91595490};
+     auto sigma2 = new TGraphErrors(3, x1, y3, ex3, ey3);
+     sigma2->SetFillColor(5);
+     sigma2->SetFillStyle(3001);
+     sigma2->GetXaxis()->SetTitle("c#tau (mm)");
+     sigma2->GetYaxis()->SetTitle("Signal strength (r)");
+     sigma2->GetXaxis()->SetRangeUser(0,10);
+     sigma2->SetTitle(" ");
+     sigma2->Draw("a3");
+     
+     //1 sigma band
+     double y4[3] = {0.03444700,0.04362106,0.41502756};//,25.23156050};
+     double ex4[3] = {0., 0., 0.};//, 0.};
+     double ey4[3] = {0.01427847,0.01799699,0.14057155};//,4.33826450};
+     auto sigma1 = new TGraphErrors(3, x1, y4, ex4, ey4);
+     sigma1->SetFillColor(8);
+     sigma1->SetFillStyle(3001);
+     sigma1->Draw("3 same");
+   
+     //observed
+     double y1[3] = {0.0369502, 0.0479211, 0.477744};//,37.8765 }; //value
+     double ex1[3] = {0., 0., 0.};//, 0.};
+     double ey1[3] = {0.00708996, 0.00871479, 0.00552663};//,0.438288  }; //error
+     auto obs = new TGraphErrors(3, x1, y1, ex1, ey1);
+     obs->SetMarkerColor(4); //blue
+     obs->SetMarkerStyle(21); //square
+     obs->SetLineColor(4);
+     obs->Draw("lp same");
+   
+     //expected (median)
+     double y2[3] = {0.0290654, 0.0383966, 0.383998};//,29.1891}; //value
+     double ex2[3] = {0., 0., 0.};//, 0.};
+     double ey2[3] = {0.00549035, 0.000545785, 0.00417906};//,0.380725}; //error
+     auto exp = new TGraphErrors(3, x1, y2, ex2, ey2);
+     exp->SetMarkerColor(1); //black
+     exp->SetMarkerStyle(21);
+     exp->SetLineStyle(9);
+     exp->SetLineColor(1); 
+     exp->Draw("lp same");
+     
+     TLegend *legend1 = new TLegend(0.1,0.7,0.3,0.9);
+     legend1->SetBorderSize(1);
+     TLegendEntry *legenda = legend1->AddEntry("obs","Observed","LP");
+     legenda->SetMarkerStyle(21);
+     legenda->SetMarkerColor(4);
+     legenda=legend1->AddEntry("exp","Expected","LP");
+     legenda->SetMarkerStyle(21);
+     legenda->SetMarkerColor(1);
+     legenda->SetLineStyle(9);
+     legenda=legend1->AddEntry("sigma1","#pm 1#sigma band","F");
+     legenda->SetFillColor(8);
+     legenda->SetFillStyle(3001);
+     legenda->SetMarkerColor(8);
+     legenda=legend1->AddEntry("sigma2","#pm 2#sigma band","F");
+     legenda->SetFillColor(5);
+     legenda->SetFillStyle(3001);
+     legenda->SetMarkerColor(5);
+     legend1->Draw();
+   
+     canv1->Modified();
+     canv1->cd();
+     canv1->SetSelected(canv1);
+     canv1->Draw(); // draw canvas!
+
+     // save canvas with drawn histogram
+     canv1->SaveAs("Limits_1GeV.gif"); // .pdf and other formats work too! 
+  }
