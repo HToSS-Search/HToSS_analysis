35051b63e30d3d846c8102d740895b97cf823bfd
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2021-06-27 14:40:36 +0200

    Create RelIso_T.C

---
+++ scripts/RelIso_T.C
@@ -0,0 +1,167 @@
+#include "TFile.h"
+
+ #include "TCanvas.h"
+ #include "TROOT.h"
+ #include "TH1F.h"
+ #include "TLegend.h"
+ #include "TLegendEntry.h"
+ #include "TStyle.h"
+ #include "TString.h"
+ #include "TLatex.h"
+ #include "TPRegexp.h"
+
+  void RelIso_T() {  
+  
+     TFile inFile0("ggH_MS2_ctau0.root"); // read in file. Exercise: can this be done through a command line arguement? That way you don't have to edit code for each new histogram ...
+     TH1F* h_PIsoSum1_ctau0    = (TH1F*)inFile0.Get("h_PIsoSum1");
+     TH1F* h_PIsoSum2_ctau0    = (TH1F*)inFile0.Get("h_PIsoSum2");
+     TH1F* h_KIsoSum1_ctau0    = (TH1F*)inFile0.Get("h_KIsoSum1");
+     TH1F* h_KIsoSum2_ctau0    = (TH1F*)inFile0.Get("h_KIsoSum2");
+     TH1F* h_MuonIsoSum1_ctau0    = (TH1F*)inFile0.Get("h_MuonIsoSum1");
+     TH1F* h_MuonIsoSum2_ctau0    = (TH1F*)inFile0.Get("h_MuonIsoSum2");
+  
+   
+     
+     TFile inFile10("ggH_MS2_ctau10.root"); 
+     TH1F* h_PIsoSum1_ctau10    = (TH1F*)inFile10.Get("h_PIsoSum1");
+     TH1F* h_PIsoSum2_ctau10    = (TH1F*)inFile10.Get("h_PIsoSum2");
+     TH1F* h_KIsoSum1_ctau10    = (TH1F*)inFile10.Get("h_KIsoSum1");
+     TH1F* h_KIsoSum2_ctau10    = (TH1F*)inFile10.Get("h_KIsoSum2");
+     TH1F* h_MuonIsoSum1_ctau10    = (TH1F*)inFile10.Get("h_MuonIsoSum1");
+     TH1F* h_MuonIsoSum2_ctau10    = (TH1F*)inFile10.Get("h_MuonIsoSum2");
+   
+     TCanvas* canv1 = new TCanvas ("canv1", "canv1", 50, 50, 800, 600); // Canvas to draw histogram on
+     gStyle->SetOptStat(0);
+     canv1->SetLogy();
+     canv1->SetLineColor(0);
+     canv1->SetFillColor(0);
+   
+     //h_PIsoSum1_ctau0->SetTitle("Leading pion, relative isolation");
+     h_PIsoSum1_ctau10->GetYaxis()->SetTitle("Events");
+     h_PIsoSum1_ctau10->GetXaxis()->SetRangeUser(0.0, 2.); 
+     h_PIsoSum1_ctau10->GetXaxis()->SetTitle("Relative isolation");
+     h_PIsoSum1_ctau10->SetMarkerColor(4);
+     h_PIsoSum1_ctau10->SetLineColor(4);
+     h_PIsoSum1_ctau10->Draw("HIST");
+ 
+     h_PIsoSum2_ctau10->GetYaxis()->SetTitle("Events");
+     h_PIsoSum2_ctau10->GetXaxis()->SetRangeUser(0.0, 2.); 
+     h_PIsoSum2_ctau10->GetXaxis()->SetTitle("Relative isolation");
+     h_PIsoSum2_ctau10->SetMarkerColor(2);
+     h_PIsoSum2_ctau10->SetLineColor(2);
+     h_PIsoSum2_ctau10->Draw("HIST same");
+   
+     TLegend *legend1 = new TLegend(0.7,0.7,0.9,0.9);
+     legend1->SetBorderSize(1);
+     TLegendEntry *legenda = legend1->AddEntry("h_PIsoSum1_ctau10","Leading","L");
+     legenda->SetLineColor(4);
+     legenda->SetMarkerColor(4);
+     legenda->SetMarkerSize(2);
+     legenda=legend1->AddEntry("h_PIsoSum2_ctau10","Subleading","L");
+     legenda->SetLineColor(2);
+     legenda->SetMarkerColor(2);
+     legenda->SetMarkerSize(2);
+     legend1->Draw();
+   
+     canv1->Modified();
+     canv1->cd();
+     canv1->SetSelected(canv1);
+     canv1->Draw(); // draw canvas!
+
+     // save canvas with drawn histogram
+     canv1->SaveAs("h_PRelIso.png"); // .pdf and other formats work too! 
+   
+    
+    
+    
+    
+    
+    
+     TCanvas* canv2 = new TCanvas ("canv2", "canv2", 50, 50, 800, 600); // Canvas to draw histogram on
+     gStyle->SetOptStat(0);
+     canv2->SetLogy();
+     canv2->SetLineColor(0);
+     canv2->SetFillColor(0);
+   
+     h_KIsoSum1_ctau10->GetYaxis()->SetTitle("Events");
+     h_KIsoSum1_ctau10->GetXaxis()->SetRangeUser(0.0, 2.); 
+     h_KIsoSum1_ctau10->GetXaxis()->SetTitle("Relative isolation");
+     h_KIsoSum1_ctau10->SetMarkerColor(4);
+     h_KIsoSum1_ctau10->SetLineColor(4);
+     h_KIsoSum1_ctau10->Draw("HIST");
+ 
+     h_KIsoSum2_ctau10->GetYaxis()->SetTitle("Events");
+     h_KIsoSum2_ctau10->GetXaxis()->SetRangeUser(0.0, 2.); 
+     h_KIsoSum2_ctau10->GetXaxis()->SetTitle("Relative isolation");
+     h_KIsoSum2_ctau10->SetMarkerColor(2);
+     h_KIsoSum2_ctau10->SetLineColor(2);
+     h_KIsoSum2_ctau10->Draw("HIST same");
+   
+     TLegend *legend2 = new TLegend(0.7,0.7,0.9,0.9);
+     legend2->SetBorderSize(1);
+     TLegendEntry *legendb = legend2->AddEntry("h_KIsoSum1_ctau10","Leading","L");
+     legendb->SetLineColor(4);
+     legendb->SetMarkerColor(4);
+     legendb->SetMarkerSize(2);
+     legendb=legend2->AddEntry("h_KIsoSum2_ctau10","Subleading","L");
+     legendb->SetLineColor(2);
+     legendb->SetMarkerColor(2);
+     legendb->SetMarkerSize(2);
+     legend2->Draw();
+   
+     canv2->Modified();
+     canv2->cd();
+     canv2->SetSelected(canv2);
+     canv2->Draw(); // draw canvas!
+
+     // save canvas with drawn histogram
+     canv2->SaveAs("h_KRelIso.png"); // .pdf and other formats work too! 
+   
+    
+    
+    
+    
+    
+     
+     TCanvas* canv3 = new TCanvas ("canv3", "canv3", 50, 50, 800, 600); // Canvas to draw histogram on
+     gStyle->SetOptStat(0);
+     canv3->SetLogy();
+     canv3->SetLineColor(0);
+     canv3->SetFillColor(0);
+   
+     h_MuonIsoSum1_ctau10->GetYaxis()->SetTitle("Events");
+     h_MuonIsoSum1_ctau10->GetXaxis()->SetRangeUser(0.0, 2.); 
+     h_MuonIsoSum1_ctau10->GetXaxis()->SetTitle("Relative isolation");
+     h_MuonIsoSum1_ctau10->SetMarkerColor(4);
+     h_MuonIsoSum1_ctau10->SetLineColor(4);
+     h_MuonIsoSum1_ctau10->Draw("HIST");
+ 
+     h_MuonIsoSum2_ctau10->GetYaxis()->SetTitle("Events");
+     h_MuonIsoSum2_ctau10->GetXaxis()->SetRangeUser(0.0, 2.); 
+     h_MuonIsoSum2_ctau10->GetXaxis()->SetTitle("Relative isolation");
+     h_MuonIsoSum2_ctau10->SetMarkerColor(2);
+     h_MuonIsoSum2_ctau10->SetLineColor(2);
+     h_MuonIsoSum2_ctau10->Draw("HIST same");
+   
+     TLegend *legend3 = new TLegend(0.7,0.7,0.9,0.9);
+     legend3->SetBorderSize(1);
+     TLegendEntry *legendc = legend3->AddEntry("h_MuonIsoSum1_ctau10","Leading","L");
+     legendc->SetLineColor(4);
+     legendc->SetMarkerColor(4);
+     legendc->SetMarkerSize(2);
+     legendc=legend3->AddEntry("h_MuonIsoSum2_ctau10","Subleading","L");
+     legendc->SetLineColor(2);
+     legendc->SetMarkerColor(2);
+     legendc->SetMarkerSize(2);
+     legend3->Draw();
+   
+     canv3->Modified();
+     canv3->cd();
+     canv3->SetSelected(canv3);
+     canv3->Draw(); // draw canvas!
+
+     // save canvas with drawn histogram
+     canv2->SaveAs("h_MuonRelIso.png"); // .pdf and other formats work too! 
+    
+    
+    
