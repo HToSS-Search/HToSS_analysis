0300f55ac9fb03a5018f57c8a1c67888678128d4
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2021-06-28 10:07:09 +0200

    Update and rename examplePlotMacro.C to higgsAssumption.C

---
+++ scripts/higgsAssumption.C
@@ -0,0 +1,29 @@
+ #include "TFile.h"
+
+ #include "TCanvas.h"
+ #include "TROOT.h"
+ #include "TH1F.h"
+ #include "TLegend.h"
+ #include "TStyle.h"
+ #include "TLatex.h"
+
+  void higgsAssumption() {  
+  
+     TFile inFile("ggH_MS2_ctau10.root"); 
+     TH2I* h_higgsassump = (TH2I*)inFile.Get("h_higgsassump"); // Load in histogram
+   
+     TCanvas* canv2 = new TCanvas ("canv2", "canv2", 50, 50, 800, 600); // Canvas to draw histogram on
+   
+     gStyle->SetOptStat(0);
+     
+     h_higgsassump->GetXaxis()->SetTitle("Pion mass assumption"); // set a title for the x-axis
+     h_higgsassump->GetXaxis()->SetRangeUser(0., 200.); // change the x-axis range to 0-200
+     h_higgsassump->GetYaxis()->SetTitle("Kaon mass assumption"); 
+     h_higgsassump->GetYaxis()->SetRangeUser(0., 200.); //
+ 
+     h_VertexPosXY->Draw("colz"); // draw histo on canvas
+     canv2->Draw(); // draw canvas!
+
+     // save canvas with drawn histogram
+     canv2->SaveAs(" h_higgsassump.png"); 
+     }
