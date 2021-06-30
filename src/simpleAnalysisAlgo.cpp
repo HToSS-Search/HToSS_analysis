6ca37ac0431f9998dc66ca680f919ded9167cc4c
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2021-06-30 20:01:35 +0200

    Update simpleAnalysisAlgo.cpp

--- src/simpleAnalysisAlgo.cpp
+++ src/simpleAnalysisAlgo.cpp
@@ -50,7 +50,7 @@ SimpleAnalysis::SimpleAnalysis()
 , isoConeSize_{0.3}          // Isolation cone size of 0.3 is used repeatedly. Define once here for efficiency.
 , higgsMass_{125.35}         // Standard Higgs mass - define here rather than multiple locations
 , higgsMassWindow_{3.0}      // Define Standard Higgs mass +/ window here rather than in multiple locations
-, higgsMassWideWindow_{20.0} // Define wider Standard Higgs mass +/ window here rather than in multiple locations
+, higgsMassWideWindow_{30.0} // Define wider Standard Higgs mass +/ window here rather than in multiple locations
 , statWindow_{0.15} 
 , mass_range_{1.0}              // 1 or 2 GeV sample run
 , debug_{false}              // Set to true to enable debug couts
@@ -234,7 +234,7 @@ void SimpleAnalysis::runMainAnalysis() {
     } // End loop over all events
 
     Float_t Nbg;
-    Nbg=(Nbg1+Nbg2)*6/34;
+    Nbg=(Nbg1+Nbg2)*6/54;
     std::cout<<"Background estimate: "<<Nbg<<std::endl;
     std::cout<<"Sideband left: "<<Nbg1<<std::endl; std::cout<<"Sideband right: "<<Nbg2<<std::endl;
     std::cout<<"Observation: "<<Obs<<std::endl;
@@ -1132,7 +1132,7 @@ void SimpleAnalysis::fillPackedCandidatePlots(const AnalysisEvent& event, double
 	
   //Background estimate
   if(KIsoSum1/event.packedCandsPseudoTrkPt[chsIndex1]<0.4 && KIsoSum2/event.packedCandsPseudoTrkPt[chsIndex2]<1 && MuonIsoSum1/event.packedCandsPseudoTrkPt[muIndex1]<0.4 && MuonIsoSum2/event.packedCandsPseudoTrkPt[muIndex2]<1){ 
-    if((Kantiscalar+scalarLVec).M()>105 && (Kantiscalar+scalarLVec).M()<122){
+    if((Kantiscalar+scalarLVec).M()>95 && (Kantiscalar+scalarLVec).M()<122){
       if(std::abs((Kantiscalar).M()-(scalarLVec).M())<statWindow_){
         Nbg1+=1;//eventWeight;
       }
@@ -1140,7 +1140,7 @@ void SimpleAnalysis::fillPackedCandidatePlots(const AnalysisEvent& event, double
   }
  
   if(KIsoSum1/event.packedCandsPseudoTrkPt[chsIndex1]<0.4 && KIsoSum2/event.packedCandsPseudoTrkPt[chsIndex2]<1 && MuonIsoSum1/event.packedCandsPseudoTrkPt[muIndex1]<0.4 && MuonIsoSum2/event.packedCandsPseudoTrkPt[muIndex2]<1){ 
-    if((Kantiscalar+scalarLVec).M()>128 && (Kantiscalar+scalarLVec).M()<145){
+    if((Kantiscalar+scalarLVec).M()>128 && (Kantiscalar+scalarLVec).M()<155){
       if(std::abs((Kantiscalar).M()-(scalarLVec).M())<statWindow_){
         Nbg2+=1;//eventWeight;
       }
