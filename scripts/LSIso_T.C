c7ad164d117a922ac4c62fff3eda6837f58b1196
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2021-07-03 13:35:59 +0200

    Update LSIso_T.C

--- scripts/LSIso_T.C
+++ scripts/LSIso_T.C
@@ -13,18 +13,18 @@
   void LSIso_T() {  
   
      TFile inFile0("ggH_MS2_ctau0.root"); // read in file. Exercise: can this be done through a command line arguement? That way you don't have to edit code for each new histogram ...
-     TH1F* h_PIsoSum1_ctau0    = (TH1F*)inFile0.Get("h_PIsoSum1");
-     TH1F* h_PIsoSum2_ctau0    = (TH1F*)inFile0.Get("h_MuonIsoSum1");
+     TH1F* h_PIsoSum2_ctau0    = (TH1F*)inFile0.Get("h_PIsoSum1");
+     TH1F* h_PIsoSum1_ctau0    = (TH1F*)inFile0.Get("h_MuonIsoSum1");
 
-     TH1F* h_MuonIsoSum1_ctau0    = (TH1F*)inFile0.Get("h_PIsoSum2");
-     TH1F* h_MuonIsoSum2_ctau0    = (TH1F*)inFile0.Get("h_MuonIsoSum2");
+     TH1F* h_MuonIsoSum2_ctau0    = (TH1F*)inFile0.Get("h_PIsoSum2");
+     TH1F* h_MuonIsoSum1_ctau0    = (TH1F*)inFile0.Get("h_MuonIsoSum2");
      
      TFile inFile10("ggH_MS2_ctau10.root"); 
-     TH1F* h_PIsoSum1_ctau10    = (TH1F*)inFile10.Get("h_PIsoSum1");
-     TH1F* h_PIsoSum2_ctau10    = (TH1F*)inFile10.Get("h_MuonIsoSum1");
+     TH1F* h_PIsoSum2_ctau10    = (TH1F*)inFile10.Get("h_PIsoSum1");
+     TH1F* h_PIsoSum1_ctau10    = (TH1F*)inFile10.Get("h_MuonIsoSum1");
 
-     TH1F* h_MuonIsoSum1_ctau10    = (TH1F*)inFile10.Get("h_PIsoSum2");
-     TH1F* h_MuonIsoSum2_ctau10    = (TH1F*)inFile10.Get("h_MuonIsoSum2");
+     TH1F* h_MuonIsoSum2_ctau10    = (TH1F*)inFile10.Get("h_PIsoSum2");
+     TH1F* h_MuonIsoSum1_ctau10    = (TH1F*)inFile10.Get("h_MuonIsoSum2");
    
      TCanvas* canv1 = new TCanvas ("canv1", "canv1", 50, 50, 800, 600); // Canvas to draw histogram on
      gStyle->SetOptStat(0);
@@ -35,6 +35,7 @@
      h_PIsoSum1_ctau10->SetTitle("");
      h_PIsoSum1_ctau10->GetYaxis()->SetTitle("Events");
      h_PIsoSum1_ctau10->GetXaxis()->SetRangeUser(0.0, 2.); 
+     h_PIsoSum1_ctau10->GetYaxis()->SetRangeUser(0.0, 15.); 
      h_PIsoSum1_ctau10->GetXaxis()->SetTitle("Relative isolation");
      h_PIsoSum1_ctau10->SetMarkerColor(4);
      h_PIsoSum1_ctau10->SetLineColor(4);
