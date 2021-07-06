3999914a3192ccaed365fb61ca61946519ee9500
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2021-07-06 10:35:32 +0200

    Update recoHiggs_pion.C

--- scripts/recoHiggs_pion.C
+++ scripts/recoHiggs_pion.C
@@ -25,7 +25,7 @@
      canv1->SetLineColor(0);
      canv1->SetFillColor(0);
 
-     //h_higgsInvMass_ctau0->GetXaxis()->SetRangeUser(0, 5); 
+     h_higgsInvMass_ctau0->GetXaxis()->SetRangeUser(0, 5); 
      h_higgsInvMass_ctau0->SetTitle("");
      h_higgsInvMass_ctau0->GetYaxis()->SetTitle("Events");
      //h_higgsInvMass_ctau0->GetXaxis()->SetRangeUser(0,200);  
