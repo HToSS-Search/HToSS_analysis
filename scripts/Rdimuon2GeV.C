b23dcc880441877ce54dddbaf61e71a814a9940a
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2021-07-14 18:08:29 +0200

    Update Rdimuon2GeV.C

--- scripts/Rdimuon2GeV.C
+++ scripts/Rdimuon2GeV.C
@@ -31,7 +31,7 @@
      canv1->SetLineColor(0);
      canv1->SetFillColor(0);
 
-     h_scalarInvMass_ctau0->GetYaxis()->SetRangeUser(0.1, 65000.);
+     h_scalarInvMass_ctau0->GetYaxis()->SetRangeUser(0.001, 10000.);
      h_scalarInvMass_ctau0->SetTitle("");
      h_scalarInvMass_ctau0->GetYaxis()->SetTitle("Events");
      h_scalarInvMass_ctau0->GetXaxis()->SetRangeUser(0,4);  
