ab943559cef820abff0f828f4ab542430a329f51
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2021-07-14 17:42:29 +0200

    Update Rdimuon2GeV.C

--- scripts/Rdimuon2GeV.C
+++ scripts/Rdimuon2GeV.C
@@ -19,6 +19,12 @@
      TFile inFile10("ggH_MS2_ctau10.root"); 
      TH1F* h_scalarInvMass_ctau10    = (TH1F*)inFile10.Get("h_RTestscalarInvMass");
  
+     TFile inFileQCD("ggH_MC_QCD.root"); 
+     TH1F* h_scalarInvMass_QCD    = (TH1F*)inFileQCD.Get("h_RTestscalarInvMass");
+       
+     TFile inFileData("dataRunD.root"); 
+     TH1F* h_scalarInvMass_data    = (TH1F*)inFileData.Get("h_RTestscalarInvMass");
+  
      TCanvas* canv1 = new TCanvas ("canv1", "canv1", 50, 50, 800, 600); // Canvas to draw histogram on
      gStyle->SetOptStat(0);
      canv1->SetLogy();
@@ -41,6 +47,22 @@
      h_scalarInvMass_ctau10->SetMarkerColor(3);
      h_scalarInvMass_ctau10->SetLineColor(3);
      h_scalarInvMass_ctau10->Draw("HIST same");
+   
+     h_scalarInvMass_QCD->SetTitle("");
+     h_scalarInvMass_QCD->GetYaxis()->SetTitle("Events");
+     h_scalarInvMass_QCD->GetXaxis()->SetRangeUser(0,4); 
+     h_scalarInvMass_QCD->GetXaxis()->SetTitle("m_{#mu#mu} (GeV/c^{2})");
+     h_scalarInvMass_QCD->SetMarkerColor(6);
+     h_scalarInvMass_QCD->SetLineColor(6);
+     h_scalarInvMass_QCD->Draw("HIST same");
+   
+     h_scalarInvMass_data->SetTitle("");
+     h_scalarInvMass_data->GetYaxis()->SetTitle("Events");
+     h_scalarInvMass_data->GetXaxis()->SetRangeUser(0,4); 
+     h_scalarInvMass_data->GetXaxis()->SetTitle("m_{#mu#mu} (GeV/c^{2})");
+     h_scalarInvMass_data->SetMarkerColor(94);
+     h_scalarInvMass_data->SetLineColor(94);
+     h_scalarInvMass_data->Draw("P same");
     
      TLegend *legend1 = new TLegend(0.7,0.7,0.9,0.9);
      legend1->SetBorderSize(1);
@@ -51,6 +73,12 @@
      legenda=legend1->AddEntry("h_scalarInvMass_ctau10","c_{#tau}=10mm","L");
      legenda->SetLineColor(3);
      legenda->SetMarkerColor(3);
+     legenda=legend1->AddEntry("h_scalarInvMass_ctau10","QCD","L");
+     legenda->SetLineColor(6);
+     legenda->SetMarkerColor(6);
+     legenda=legend1->AddEntry("h_scalarInvMass_ctau10","Observation","P");
+     legenda->SetLineColor(94);
+     legenda->SetMarkerColor(94);
      legenda->SetMarkerSize(2);
 
      legend1->Draw();
