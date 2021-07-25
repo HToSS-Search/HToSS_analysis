8cd733c064dd4d3f8520dcbde11e7fec7aef0c05
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2021-07-25 11:03:44 +0200

    Update RrecoHiggs_kaon.C

--- scripts/RrecoHiggs_kaon.C
+++ scripts/RrecoHiggs_kaon.C
@@ -34,7 +34,7 @@
      //h_refit12InvMass_ctau0->GetYaxis()->SetRangeUser(1.0, 65000.);
      h_higgsInvMass_ctau0->SetTitle("");
      h_higgsInvMass_ctau0->GetYaxis()->SetTitle("Events");
-     h_higgsInvMass_ctau0->GetXaxis()->SetRangeUser(0,200);  
+     h_higgsInvMass_ctau0->GetXaxis()->SetRangeUser(80,200);  
      h_higgsInvMass_ctau0->GetXaxis()->SetTitle("m_{Higgs} (GeV/c^{2})");
      h_higgsInvMass_ctau0->SetMarkerColor(4);
      h_higgsInvMass_ctau0->SetLineColor(4);
@@ -42,7 +42,7 @@
  
      h_higgsInvMass_ctau10->SetTitle("");
      h_higgsInvMass_ctau10->GetYaxis()->SetTitle("Events");
-     h_higgsInvMass_ctau10->GetXaxis()->SetRangeUser(0,200); 
+     h_higgsInvMass_ctau10->GetXaxis()->SetRangeUser(80,200); 
      h_higgsInvMass_ctau10->GetXaxis()->SetTitle("m_{Higgs} (GeV/c^{2})");
      h_higgsInvMass_ctau10->SetMarkerColor(3);
      h_higgsInvMass_ctau10->SetLineColor(3);
@@ -50,7 +50,7 @@
     
      h_higgsInvMass_QCD->SetTitle("");
      h_higgsInvMass_QCD->GetYaxis()->SetTitle("Events");
-     h_higgsInvMass_QCD->GetXaxis()->SetRangeUser(0,200); 
+     h_higgsInvMass_QCD->GetXaxis()->SetRangeUser(80,200); 
      h_higgsInvMass_QCD->GetXaxis()->SetTitle("m_{Higgs} (GeV)");
      h_higgsInvMass_QCD->SetMarkerColor(6);
      h_higgsInvMass_QCD->SetLineColor(6);
@@ -58,7 +58,7 @@
    
      h_higgsInvMass_data->SetTitle("");
      h_higgsInvMass_data->GetYaxis()->SetTitle("Events");
-     h_higgsInvMass_data->GetXaxis()->SetRangeUser(0,200); 
+     h_higgsInvMass_data->GetXaxis()->SetRangeUser(80,200); 
      h_higgsInvMass_data->GetXaxis()->SetTitle("m_{Higgs} (GeV)");
      h_higgsInvMass_data->SetMarkerColor(94);
      h_higgsInvMass_data->SetMarkerStyle(21);
