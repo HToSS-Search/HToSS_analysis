aefdc338667f633d7daae3e5d8315ff742358c01
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2021-08-12 09:43:24 +0200

    Update triggerLeading.C

--- scripts/triggerLeading.C
+++ scripts/triggerLeading.C
@@ -70,6 +70,6 @@
      canv1->Draw(); // draw canvas!
 
      // save canvas with drawn histogram
-     canv1->SaveAs("triggerLeading.png"); // .pdf and other formats work too! 
+     canv1->SaveAs("triggerLeading.pdf"); // .pdf and other formats work too! 
    
   }
