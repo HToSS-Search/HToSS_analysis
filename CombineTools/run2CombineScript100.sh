d28922b69cc0374b362791223d4be4f786f96d04
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2021-08-13 11:03:54 +0200

    Create run2CombineScript100.sh

---
+++ CombineTools/run2CombineScript100.sh
@@ -0,0 +1,6 @@
+combine datacard_2GeV_100mm.txt -M HybridNew -H ProfileLikelihood --frequentist --testStat LHC --fork 4  -T 5000 --rAbsAcc=0.01 --name output_name
+combine datacard_2GeV_100mm.txt -M HybridNew -H ProfileLikelihood --frequentist --testStat LHC --expectedFromGrid=0.5 --fork 4  -T 5000 --rAbsAcc=0.01 --name output_name
+combine datacard_2GeV_100mm.txt -M HybridNew -H ProfileLikelihood --frequentist --testStat LHC --expectedFromGrid=0.84 --fork 4  -T 5000 --rAbsAcc=0.01 --name output_name
+combine datacard_2GeV_100mm.txt -M HybridNew -H ProfileLikelihood --frequentist --testStat LHC --expectedFromGrid=0.16 --fork 4  -T 5000 --rAbsAcc=0.01 --name output_name
+combine datacard_2GeV_100mm.txt -M HybridNew -H ProfileLikelihood --frequentist --testStat LHC --expectedFromGrid=0.975 --fork 4  -T 5000 --rAbsAcc=0.01 --name output_name
+combine datacard_2GeV_100mm.txt -M HybridNew -H ProfileLikelihood --frequentist --testStat LHC --expectedFromGrid=0.025 --fork 4  -T 5000 --rAbsAcc=0.01 --name output_name
