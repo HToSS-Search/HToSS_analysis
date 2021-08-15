db52dd17b275d111cd9db7fa5eaea8ac7daeb69c
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2021-08-15 16:03:45 +0200

    Create data2GeV.sh

---
+++ CombineTools/data2GeV.sh
@@ -0,0 +1,4 @@
+./bin/simpleAnalysis.exe -c configs/2017/ggH_MS2_ctauS0_Conf.yaml -o ggH_MS2_ctau0S.root -u
+./bin/simpleAnalysis.exe -c configs/2017/ggH_MS2_ctauS1_Conf.yaml -o ggH_MS2_ctau1S.root -u
+./bin/simpleAnalysis.exe -c configs/2017/ggH_MS2_ctauS10_Conf.yaml -o ggH_MS2_ctau10S.root -u
+./bin/simpleAnalysis.exe -c configs/2017/ggH_MS2_ctauS100_Conf.yaml -o ggH_MS2_ctau100S.root -u
