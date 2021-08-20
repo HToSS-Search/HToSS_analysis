e638e8129ccc2748e88af95f97e312951d650074
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2021-08-20 13:07:51 +0200

    Update dimuon2GeV.C

--- scripts/dimuon2GeV.C
+++ scripts/dimuon2GeV.C
@@ -63,6 +63,6 @@
      canv1->Draw(); // draw canvas!
 
      // save canvas with drawn histogram
-     canv1->SaveAs("h_dimuon2GeV.pdf"); // .pdf and other formats work too! 
+     canv1->SaveAs("h_dimuon2GeV.png"); // .pdf and other formats work too! 
    
   }
