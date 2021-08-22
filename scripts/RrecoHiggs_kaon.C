1fb4822bbff92fe5332e6a57435c5a1ba8d5edae
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2021-08-22 14:47:11 +0200

    Update RrecoHiggs_kaon.C

--- scripts/RrecoHiggs_kaon.C
+++ scripts/RrecoHiggs_kaon.C
@@ -41,6 +41,7 @@
      h_higgsInvMass_ctau0->SetLineColor(4);
      h_higgsInvMass_ctau0->Draw("HIST");
  
+     h_higgsInvMass_ctau10->GetYaxis()->SetRangeUser(0.01,1000);
      h_higgsInvMass_ctau10->SetTitle("");
      h_higgsInvMass_ctau10->GetYaxis()->SetTitle("Events");
      h_higgsInvMass_ctau10->GetXaxis()->SetRangeUser(80,170); 
@@ -50,6 +51,7 @@
      h_higgsInvMass_ctau10->Draw("HIST");
     
      h_higgsInvMass_QCD->SetTitle("");
+     h_higgsInvMass_QCD->GetYaxis()->SetRangeUser(0.01,1000);
      h_higgsInvMass_QCD->GetYaxis()->SetTitle("Events");
      h_higgsInvMass_QCD->GetXaxis()->SetRangeUser(80,170); 
      h_higgsInvMass_QCD->GetXaxis()->SetTitle("m_{Higgs} (GeV)");
@@ -58,6 +60,7 @@
      h_higgsInvMass_QCD->Draw("HIST same");
    
      h_higgsInvMass_data->SetTitle("");
+     h_higgsInvMass_data->GetYaxis()->SetRangeUser(0.01,1000);
      h_higgsInvMass_data->GetYaxis()->SetTitle("Events");
      h_higgsInvMass_data->GetXaxis()->SetRangeUser(80,170); 
      h_higgsInvMass_data->GetXaxis()->SetTitle("m_{Higgs} (GeV)");
