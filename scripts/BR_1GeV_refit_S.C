22a6e9132e6dbec795382a4fe8a33d0f0ef379d9
cbossuyt <71316258+cbossuyt@users.noreply.github.com>
2021-08-22 12:46:35 +0200

    Create BR_1GeV_refit_S.C

---
+++ scripts/BR_1GeV_refit_S.C
@@ -0,0 +1,127 @@
+#include "TFile.h"
+ #include "TCanvas.h"
+ #include "TROOT.h"
+ #include "TGraphPainter.h"
+ #include "TLegend.h"
+ #include "TLegendEntry.h"
+ #include "TStyle.h"
+ #include "TLine.h"
+ #include "TString.h"
+ #include "TLatex.h"
+ #include "TPRegexp.h"
+ #include "TAttLine.h"
+
+  void BR_1GeV_refit_S() {  
+  
+     TCanvas* canv1 = new TCanvas ("canv1", "canv1", 50, 50, 800, 600); // Canvas to draw histogram on
+     canv1->SetGrid();
+     canv1->SetLogy(); canv1->SetLogx();
+   
+     double x1[4] = {0, 1, 10, 100};
+    
+     //2 sigma band
+     double y3[4] = {74.1762751950000,112.904262750000,1559.12740275000,142477.268130000}; 
+     double ex3[4] = {0., 0., 0., 0.};
+     double ey3[4] = {40.5844657650000,61.5565434600000,852.048919350000,87078.4400700000};
+     auto sigma2 = new TGraphErrors(4, x1, y3, ex3, ey3);
+     sigma2->SetFillColor(5);
+     sigma2->SetFillStyle(3001);
+     sigma2->GetXaxis()->SetTitle("c#tau (mm)");
+     sigma2->GetYaxis()->SetTitle("BR(H#rightarrow a#bar{a})");
+     sigma2->GetXaxis()->SetRangeUser(0,100);
+     //sigma2->GetYaxis()->SetRangeUser(-10,195585);
+     sigma2->SetTitle(" ");
+     sigma2->Draw("a3");
+     
+     //1 sigma band
+     double y4[4] = {64.1267847300000,98.4864674250000,1336.61678475000,106420.763535000};
+     double ex4[4] = {0., 0., 0., 0.};
+     double ey4[4] = {21.1419840000000,32.6148680250000,455.208689850000,36735.1750650000};
+     auto sigma1 = new TGraphErrors(4, x1, y4, ex4, ey4);
+     sigma1->SetFillColor(8);
+     sigma1->SetFillStyle(3001);
+     //sigma1->GetYaxis()->SetRangeUser(0,195585);
+     sigma1->Draw("3 same");
+   
+     //observed
+     double y1[4] = {91.4485680000000,137.753943000000,1879.07664000000,152038.209000000}; //value
+     double ex1[4] = {0., 0., 0., 0.};
+     double ey1[4] = {0.551222400000000,0.746536110000000,10.3937502000000,1418.87910000000}; //error
+     auto obs = new TGraphErrors(4, x1, y1, ex1, ey1);
+     obs->SetMarkerColor(4); //blue
+     obs->SetMarkerStyle(21); //square
+     obs->SetLineColor(4);
+     //obs->GetYaxis()->SetRangeUser(0,195585);
+     obs->Draw("lp same");
+   
+     //expected (median)
+     double y2[4] = {59.4232380000000,86.8432560000000,1246.82913000000,95824.1370000000}; //value
+     double ex2[4] = {0., 0., 0., 0.};
+     double ey2[4] = {0.940020720000000,3.70338480000000,15.3296469000000,904.021620000000}; //error
+     auto exp = new TGraphErrors(4, x1, y2, ex2, ey2);
+     exp->SetMarkerColor(1); //black
+     exp->SetMarkerStyle(21);
+     exp->SetLineStyle(9);
+     exp->SetLineColor(1);
+     //exp->GetYaxis()->SetRangeUser(0,195585);
+     exp->Draw("lp same");
+   
+     TLine *l=new TLine(0,0.34,100,0.34);
+     l->SetLineColor(2);
+     l->SetLineStyle(1);
+     l->SetLineWidth(2);
+     //l->GetYaxis()->SetRangeUser(0,195585);
+     l->Draw("same");
+  
+     //unphysical area
+     double y5[4] = {11600,11600,11600,11600}; 
+     double ex5[4] = {0., 0., 0., 0.};
+     double ey5[4] = {11599,11599,11599,11599};
+     auto sigma3 = new TGraphErrors(4, x1, y5, ex5, ey5);
+     sigma3->SetFillColor(14);
+     sigma3->SetFillStyle(3244);
+     sigma3->SetTitle(" ");
+     //sigma3->GetYaxis()->SetRangeUser(0,195585);
+     sigma3->Draw("3 same");
+     
+     TLine *l2=new TLine(0,1,100,1);
+     l2->SetLineColor(14);
+     l2->SetLineStyle(1);
+     l2->SetLineWidth(1);
+     //l2->GetYaxis()->SetRangeUser(0,195585);
+     l2->Draw("same");
+   
+     TLegend *legend1 = new TLegend(0.1,0.7,0.3,0.9);
+     legend1->SetBorderSize(1);
+     TLegendEntry *legenda = legend1->AddEntry("obs","Observed","LP");
+     legenda->SetMarkerStyle(21);
+     legenda->SetMarkerColor(4);
+     legenda=legend1->AddEntry("exp","Expected","LP");
+     legenda->SetMarkerStyle(21);
+     legenda->SetMarkerColor(1);
+     legenda->SetLineStyle(9);
+     legenda=legend1->AddEntry("sigma1","#pm 1#sigma band","F");
+     legenda->SetFillColor(8);
+     legenda->SetFillStyle(3001);
+     legenda->SetMarkerColor(8);
+     legenda=legend1->AddEntry("sigma2","#pm 2#sigma band","F");
+     legenda->SetFillColor(5);
+     legenda->SetFillStyle(3001);
+     legenda->SetMarkerColor(5);
+     legenda=legend1->AddEntry("l","Constraint BSM","L");
+     legenda->SetLineWidth(2);
+     legenda->SetLineColor(2);
+     legenda=legend1->AddEntry("l2","Unphysical area","F");
+     legenda->SetFillColor(14);
+     legenda->SetFillStyle(3244);
+     legenda->SetMarkerColor(14);
+     legend1->Draw();
+   
+     canv1->Modified();
+     canv1->cd();
+     canv1->SetSelected(canv1);
+     canv1->Draw(); // draw canvas!
+
+     // save canvas with drawn histogram
+     canv1->SaveAs("BR_1GeV_refit_S.pdf"); // .pdf and other formats work too! 
+  }
